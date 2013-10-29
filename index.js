30a48f98de2e2f9f905281bf07ac40757984e33a
Mathias Buus <mathiasbuus@gmail.com>
2013-10-29 10:02:03 +0100

    refactor

--- index.js
+++ index.js
@@ -4,142 +4,139 @@ var cmds = {};
 var options = {};
 var positionals = {};
 var aliases = {};
-var all = [];
 
-var noop = function() {};
+var toFunction = function(values) {
+	if (typeof values === 'function') return values;
 
-var unopt = function(opt) {
-	return opt.replace(/-/g, '');
+	return function(callback) {
+		callback(null, values);
+	};
 };
 
-var opt = function(name) {
-	return '--'+name;
+var clone = function(obj) {
+	var c = {};
+	Object.keys(obj || {}).forEach(function(key) {
+		c[key] = obj[key];
+	});
+	return c;
 };
 
 var normalize = function(argv) {
 	Object.keys(argv).forEach(function(arg) {
-		if (!aliases['-'+arg]) return;
-		var opt = unopt(aliases['-'+arg]);
+		if (!aliases[arg]) return;
+		var opt = aliases[arg];
 		argv[opt] = argv[opt] ? [].concat(argv[opt], argv[arg]) : argv[arg];
 		delete argv[arg];
 	});
 	return argv;
 };
 
-var toFunction = function(values) {
-	if (typeof values === 'function') return values;
-	return function(callback) {
-		callback(null, values);
-	};
+var opt = function(name) {
+	return '--'+name;
+};
+
+var deopt = function(opt) {
+	return opt.replace(/^-+/g, '');
 };
 
 var tab = function(name) {
-	if (typeof name === 'function') return tab()(name);
-
 	var index = name ? 1 : 0;
 
 	name = name || '__main__';
-	options[name] = {};
-	positionals[name] = {};
+	var opts = options[name] = clone(options['*']);
+	var pos = positionals[name] = clone(positionals['*']);
 
 	var onpositional = function(values) {
-		if (name in cmds) positionals[name][index++] = toFunction(cmds[name]);
+		if (name === '*') return pos[index++] = toFunction(values);
+		if (name in cmds) pos[index++] = toFunction(cmds[name]);
 		cmds[name] = values;
 	};
 
-	var onoption = function(long, short, values) {
-		if (typeof short !== 'string') return onoption(long, long, short);
-		aliases[short] = long;
-		options[name][unopt(long)] = toFunction(values);
+	var onoption = function(name, shorts, values) {
+		shorts.forEach(function(s) {
+			aliases[s] = name;
+		});
+		opts[name] = toFunction(values);
 	};
 
-	var opt = function(a, b, c) {
-		if (typeof a === 'string' && a[0] === '-') onoption(a, b, c);
-		else onpositional(a);
-		return opt;
+	var parse = function(a) {
+		if (typeof a !== 'string' || a[0] !== '-') {
+			onpositional(a);
+			return parse;
+		}
+		var shorts = Array.prototype.slice.call(arguments, 1);
+		var values = (shorts[shorts.length - 1] || '')[0] === '-' ? undefined : shorts.pop();
+		onoption(deopt(a), shorts.map(deopt), values);
+		return parse;
 	};
 
-	all.forEach(function(args) {
-		opt.apply(null, args);
-	});
-
-	return opt;
+	return parse;
 };
 
-tab.all = function() {
-	all.push(arguments);
-	return tab.all;
+var call = function(fn, word, opts, callback) {
+	if (fn.length === 3) return fn(word, opts, callback);
+	if (fn.length === 2) return fn(word, callback);
+	fn(callback);
 };
 
-tab.complete = function(index, words, callback) {
-	if (!cmds.__all__) tab('__all__');
-
+var complete = function(index, words) {
 	var cur = words[index] || '';
 	var prev = words[index-1] || '';
-	var argv = normalize(minimist(words.slice(0, index+1)));
-	var _ = argv._;
+	var argv = normalize(minimist(words));
+	var _ = minimist(words.slice(0, index+1))._;
 	var cmd = _[0];
 
-	var finish = function(err, values) {
-		if (err) return;
-		values = [].concat(values || []).filter(function(value) {
-			return (''+value).slice(0, cur.length) === cur;
+	var filter = function(err, values) {
+		if (err) return finish(err);
+		values = [].concat(values || []).map(String).filter(function(value) {
+			return value.slice(0, cur.length) === cur;
 		});
-		callback(null, values);
+		finish(null, values);
+
+	};
+	var finish = function(err, values) {
+		if (err) return process.exit(1);
+		console.log(values.join('\n'));
+		process.exit(0);
 	};
 
 	if (cur.slice(0, 2) === '--' || cur === '-') {
-		var names = Object.keys(options[cmd] || options.__main__ || options.__all__ || {});
-		return finish(null, names.map(opt));
+		var names = Object.keys(options[cmd] || options.__main__ || options['*'] || {});
+		return filter(null, names.map(opt));
 	}
 
-	if (aliases[cur]) return callback(null, [aliases[cur]]);
+	if (aliases[cur]) return finish(null, [aliases[cur]]);
 	if (aliases[prev]) prev = aliases[prev];
 
 	if (prev.slice(0, 2) === '--') {
 		var name = unopt(prev);
-		var fn = (options[cmd] || options.__main__ || options.__all__ || {})[name];
-		return fn ? fn.call(argv, finish) : finish();
+		var fn = (options[cmd] || options.__main__ || options['*'] || {})[name];
+		if (fn) return call(fn, cur, argv, filter);
+		return filter();
 	}
-
-	if (prev[0] === '-' || cur[0] === '-') return finish();
 
 	index = _.length-1;
-
 	var pos = (positionals[cmd] || positionals.__main__ || {})[index];
-	if (pos) return pos.call(argv, finish);
+	if (pos) return call(pos, cur, argv, filter);
 
-	if (_.length < 2) {
-		var words = Object.keys(cmds).filter(function(cmd) {
-			return cmd !== '__main__' && cmd !== '__all__';
-		});
-		return finish(null, words);
-	}
+	if (prev[0] === '-' || cur[0] === '-') return filter();
+	if (_.length > 1) return filter();
 
-	finish();
-};
+	var words = Object.keys(cmds).filter(function(cmd) {
+		return cmd !== '__main__' && cmd !== '*';
+	});
 
-tab.help = function() {
-	console.log('Unknown command');
-};
-
-tab.install = function() {
-	require('./install'); // deferred require for faster tab completion load
+	return filter(null, words);
 };
 
 tab.parse = function(argv) {
 	argv = argv || process.argv.slice(2);
 
+	if (argv[0] === '--tabalot' && argv.length === 1) {
+		return require('./install');
+	}
 	if (argv[0] === '--tabalot') {
-		if (argv.length === 1) {
-			tab.install();
-			process.exit(0);
-		}
-		tab.complete(Number(argv[1]), argv.slice(2), function(err, words) {
-			console.log((words || []).join('\n'))
-			process.exit(0);
-		});
-		return true;
+		return complete(Number(argv[1]), argv.slice(2));
 	}
 
 	argv = normalize(minimist(argv));
@@ -151,7 +148,7 @@ tab.parse = function(argv) {
 		if (!cmds[cmd]) return false;
 
 		process.nextTick(function() {
-			var arity = Object.keys(positionals[cmd || '__main__']).length;
+			var arity = Object.keys(positionals[cmd]).length;
 			var args = [];
 
 			for (var i = 0; i < arity; i++) {
@@ -164,10 +161,7 @@ tab.parse = function(argv) {
 		return true;
 	};
 
-	var cmd = argv._[0];
-
-	if (apply(cmd)) return true;
-	return apply();
+	return apply(argv._[0]) || apply();
 };
 
 module.exports = tab;
